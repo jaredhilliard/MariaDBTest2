0080179a6188610c37bafb33a1efb9f7ef4a45b4
Jared Hilliard <39249055+jaredhilliard@users.noreply.github.com>
2020-02-07 15:40:29 -0500

    Made changes to connect to our project db

--- src/main/java/MariaDBTest2.java
+++ src/main/java/MariaDBTest2.java
@@ -11,14 +11,14 @@ import java.sql.Statement;
  */
 public class MariaDBTest2 {
     public static void main(String[] args) {
-        System.out.println("asdas");
+        System.out.println("I'm gonna try to connect to the database.");
         try{
             Class.forName("com.mysql.jdbc.Driver");
             Connection con=DriverManager.getConnection(
-                    "jdbc:mysql://3.83.240.60:3306/employees","mysql","mysql");
+                    "jdbc:mysql://54.145.49.152:3306/project","mysql","mysql");
 //here sonoo is database name, root is username and password
             Statement stmt=con.createStatement();
-            ResultSet rs=stmt.executeQuery("SELECT * FROM employees");
+            ResultSet rs=stmt.executeQuery("SELECT * FROM week_data");
             while(rs.next())
                 System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
             con.close();
